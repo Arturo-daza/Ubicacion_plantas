2726d678613138699eed53f6d988533d4e2026a4
Arturo-daza <davisdaza70@gmail.com>
2023-02-27 11:16:32 -0500

    Creacion de los metodos CRUD para la tabla centroide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
+from psycopg2 import Error
  
 app = Flask(__name__)
 app.secret_key = "cairocoders-ednalan"
@@ -13,66 +14,116 @@ DB_USER = "biwoqvym"
 DB_PASS = "SgxGYDFppxH471IfeWjelmoI_MrhlnZW"
  
 ## Crear una instacia que se conecte a la base de datos
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+
+conexion = None
+def conexion(DB_HOST, DB_NAME, DB_USER, DB_PASS):
+  try:
+    con = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+  except Error as e:
+    print("Error al establecer conexión: ",e)
+  return con 
+
+#Cierre conexion
+def cierre_conexion(con):
+    try:
+        con.close()
+    except Error as e:
+        print('Se ha presentado error al cerrar la conexion')
+
+def consulta () :  
+  #Consulta de datos 
+    resultado = None
+    try:
+        con = conexion(DB_NAME=DB_NAME, DB_USER=DB_USER, DB_PASS=DB_PASS, DB_HOST=DB_HOST)
+        cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute('SELECT * FROM centroide')
+        resultado = cursor.fetchall()
+        cierre_conexion(con)        
+    except Error as e:
+        print(f'Se ha presentado un error al consultar la tabla de parametros_produccion:{e}')
+    return resultado
+
+def get_id(id):
+    con = conexion(DB_NAME=DB_NAME, DB_USER=DB_USER, DB_PASS=DB_PASS, DB_HOST=DB_HOST)
+    cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute('SELECT * FROM centroide WHERE id = %s', (id))
+    data = cursor.fetchall()
+    cierre_conexion(con) 
+    return data 
+
+def insercion (cliente, latitud, longitud, distribucion) :
+
+  con = conexion(DB_NAME=DB_NAME, DB_USER=DB_USER, DB_PASS=DB_PASS, DB_HOST=DB_HOST)
+  cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+  sql = f"INSERT INTO centroide(cliente, latitud, longitud, distribucion) VALUES('{cliente}','{latitud}','{longitud}', '{distribucion}')"
+  cursor.execute(sql)
+  #Comfirma los cambios en la base de datos
+  con.commit() 
+  cierre_conexion(con) 
+  print("Registro ingresado satisfactoriamente")   
+
+def update (cliente, latitud, longitud, distribucion, id) :  
+    con = conexion(DB_NAME=DB_NAME, DB_USER=DB_USER, DB_PASS=DB_PASS, DB_HOST=DB_HOST)
+    cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("""
+        UPDATE centroide
+        SET cliente = %s,
+            latitud = %s,
+            longitud = %s,
+            distribucion = %s
+        WHERE id = %s
+    """, (cliente, latitud, longitud, distribucion, id))
+    con.commit()
+    cierre_conexion(con)
  
+def delete (id):
+        con = conexion(DB_NAME=DB_NAME, DB_USER=DB_USER, DB_PASS=DB_PASS, DB_HOST=DB_HOST)
+        cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute('DELETE FROM centroide WHERE id = {0}'.format(id))
+        con.commit()
+        cierre_conexion(con)
+        
 @app.route('/')
 def Index():
-    ## Se crea cursos para interactuar con la base de datos
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    # Se escribe la sentencia SQL
-    s = "SELECT * FROM estudiantes"
-    cur.execute(s) # Execute the SQL
-    list_users = cur.fetchall() ## Para que muestre todos los resultados
+    list_users = consulta() 
     return render_template('index.html', list_users = list_users)
  
-@app.route('/add_student', methods=['POST'])
-def add_student():
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+@app.route('/add_register', methods=['POST'])
+def add_register():
     if request.method == 'POST':
-        nombre = request.form['nombre']
-        codigo = request.form['codigo']
-        email = request.form['email']
-        cur.execute("INSERT INTO estudiantes (nombre, codigo, email) VALUES (%s,%s,%s)", (nombre, codigo, email))
-        conn.commit()
-        flash('Estudiante añadido con exito')
+        cliente = request.form['cliente']
+        latitud = request.form['latitud']
+        longitud = request.form['longitud']
+        distribucion = request.form['distribucion']
+        insercion(cliente, latitud, longitud, distribucion)
+        flash('Registro añadido con exito')
         return redirect(url_for('Index'))
  
 @app.route('/edit/<id>', methods = ['POST', 'GET'])
 def get_employee(id):
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-   
-    cur.execute('SELECT * FROM estudiantes WHERE id = %s', (id))
-    data = cur.fetchall()
-    cur.close()
+    data = get_id(id)
     print(data[0])
-    return render_template('edit.html', student = data[0])
+    return render_template('edit.html', centroide = data[0])
+
+
  
 @app.route('/update/<id>', methods=['POST'])
-def update_student(id):
+def update_register(id):
     if request.method == 'POST':
-        nombre = request.form['nombre']
-        codigo = request.form['codigo']
-        email = request.form['email']
-         
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("""
-            UPDATE estudiantes
-            SET nombre = %s,
-                codigo = %s,
-                email = %s
-            WHERE id = %s
-        """, (nombre, codigo, email, id))
-        flash('Datos del estudiante actualizados')
-        conn.commit()
+        cliente = request.form['cliente']
+        latitud = request.form['latitud']
+        longitud = request.form['longitud']
+        distribucion = request.form['distribucion']
+        
+        update(cliente, latitud, longitud, distribucion, id)
+        flash('Datos actualizados')
         return redirect(url_for('Index'))
  
 @app.route('/delete/<string:id>', methods = ['POST','GET'])
 def delete_student(id):
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-   
-    cur.execute('DELETE FROM estudiantes WHERE id = {0}'.format(id))
-    conn.commit()
-    flash('Estudiante removido con exito')
+    delete(id)   
+    flash('Registro removido con exito')
     return redirect(url_for('Index'))
  
 if __name__ == "__main__":
